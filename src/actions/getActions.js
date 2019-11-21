290e2ff3def5353c1a3585c81ef9fa17a3b28151
wincodes <otokinaodafe@gmail.com>
2019-11-21 20:42:30 +0100

    implement create article component, view article component and functionality, create and get article actions

--- src/actions/getActions.js
+++ src/actions/getActions.js
@@ -45,3 +45,49 @@ export const getSingleGif = (gifId) => async dispatch => {
     })
   }
 }
+
+export const getSingleArticle = (articleId) => async dispatch => {
+  try {
+    const Bearer = 'Bearer ' + localStorage.jwtToken;
+
+    let response = await fetch(`${URL}/articles/${articleId}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': Bearer,
+      }
+    });
+    response = await response.json();
+
+    if (response.status === 'error') {
+      if (typeof response.error === "object") {
+        dispatch({
+          type: ERRORS,
+          payload: response.error
+        })
+      } else {
+        dispatch({
+          type: ERRORS,
+          payload: {
+            feedback: response.error
+          }
+        })
+      }
+      return response
+    } else {
+      dispatch({
+        type: ERRORS,
+        payload: {}
+      })
+
+      return response
+    }
+  } catch (error) {
+    console.log(error)
+    dispatch({
+      type: ERRORS,
+      payload: {
+        feedback: 'An error occurred, please try again'
+      }
+    })
+  }
+}
--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -51,3 +51,54 @@ export const postGif = (form, history) => async dispatch => {
     })
   }
 }
+
+export const postArticle = (articleData, history) => async dispatch => {
+  try {
+    const Bearer = 'Bearer ' + localStorage.jwtToken;
+
+    let response = await fetch(`${URL}/articles`, {
+      method: 'POST',
+      withCredentials: true,
+      body: JSON.stringify(articleData),
+      headers: {
+        'Authorization': Bearer,
+        'Content-Type': 'application/json',
+      }
+    });
+
+    response = await response.json();
+
+    if (response.status === 'error') {
+      if (typeof response.error === "object") {
+        dispatch({
+          type: ERRORS,
+          payload: response.error
+        })
+      } else {
+        dispatch({
+          type: ERRORS,
+          payload: {
+            feedback: response.error
+          }
+        })
+      }
+    } else {
+      dispatch({
+        type: ERRORS,
+        payload: {}
+      })
+
+      const { articleId } = response.data
+
+      history.push(`/articles/${articleId}`)
+    }
+  } catch (error) {
+    console.log(error)
+    dispatch({
+      type: ERRORS,
+      payload: {
+        feedback: 'An error occurred, please try again'
+      }
+    })
+  }
+}
