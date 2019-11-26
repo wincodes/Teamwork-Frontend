16b1e0e3060ac95ed9c588fd07fa79bfefe77815
wincodes <otokinaodafe@gmail.com>
2019-11-26 23:21:11 +0100

    Updates

---
+++ precache-manifest.9d027e15389d5a49c8a962bac5eb6da6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf0adc0f8e729d9e2495bb1b8b7f3ea",
+    "url": "/Teamwork-Frontend/index.html"
+  },
+  {
+    "revision": "2ae8597c3f2a33cf2f07",
+    "url": "/Teamwork-Frontend/static/css/2.4b3ccb6c.chunk.css"
+  },
+  {
+    "revision": "56cc5260748cce685ab4",
+    "url": "/Teamwork-Frontend/static/css/main.3423457e.chunk.css"
+  },
+  {
+    "revision": "2ae8597c3f2a33cf2f07",
+    "url": "/Teamwork-Frontend/static/js/2.f4bd81c5.chunk.js"
+  },
+  {
+    "revision": "56cc5260748cce685ab4",
+    "url": "/Teamwork-Frontend/static/js/main.c7203892.chunk.js"
+  },
+  {
+    "revision": "19b8e851cdcaeb24deda",
+    "url": "/Teamwork-Frontend/static/js/runtime-main.84d3ac6a.js"
+  },
+  {
+    "revision": "d0797a882ef79aec2ea015612ceb5003",
+    "url": "/Teamwork-Frontend/static/media/devc.d0797a88.jpg"
+  }
+]);
--- src/actions/types.js
+++
@@ -1,7 +0,0 @@
-export const SET_CURRENT_USER = 'SET_CURRENT_USER';
-
-export const ERRORS = 'ERRORS';
-
-export const URL = 'https://guarded-brushlands-25724.herokuapp.com/api/v1'
-
-export const NOTIFICATION = 'NOTIFICATION';
